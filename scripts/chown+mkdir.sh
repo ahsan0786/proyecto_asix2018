041b82f5594f991ddeb0908e250e602f86ff4d90
ahsan0786 <a14ahsiqbiqb@iam.cat>
2018-05-28 10:34:55 +0200

    ok

--- scripts/chown+mkdir.sh
+++ scripts/chown+mkdir.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+CARPETA=proyecto_asix2018
 cd ${HOME}
 sudo rm -r ./proyecto_asix2018
 git clone https://github.com/ahsan0786/proyecto_asix2018.git
@@ -9,15 +10,15 @@ if [ -d /home/ubuntu/docker/containers/mysql ] && [ -d /home/ubuntu/docker/conta
         sudo mkdir /home/ubuntu/docker/containers/mysql
         sudo mkdir /home/ubuntu/docker/containers/mysql-config
         sudo mkdir /home/ubuntu/docker/containers/wordpress
-        sudo cp -a ${HOME}/proyecto/config/my.cnf /home/ubuntu/docker/containers/mysql-config
-        sudo cp -a ${HOME}/proyecto/config/my2.cnf /home/ubuntu/docker/containers/mysql-config
+        sudo cp -a ${HOME}/$CARPETA/config/my.cnf /home/ubuntu/docker/containers/mysql-config
+        sudo cp -a ${HOME}/$CARPETA/config/my2.cnf /home/ubuntu/docker/containers/mysql-config
 
 else
         sudo mkdir /home/ubuntu/docker/containers/mysql
         sudo mkdir /home/ubuntu/docker/containers/mysql-config
         sudo mkdir /home/ubuntu/docker/containers/wordpress
-        sudo cp -a ${HOME}/proyecto/config/my.cnf /home/ubuntu/docker/containers/mysql-config
-        sudo cp -a ${HOME}/proyecto/config/my2.cnf /home/ubuntu/docker/containers/mysql-config
+        sudo cp -a ${HOME}/$CARPETA/config/my.cnf /home/ubuntu/docker/containers/mysql-config
+        sudo cp -a ${HOME}/$CARPETA/config/my2.cnf /home/ubuntu/docker/containers/mysql-config
 
 fi
 
